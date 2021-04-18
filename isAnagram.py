8274c2e2cb3672ff3735a454f7f610c51c5ae26f
rohi92 <gorthyrohith@gmail.com>
2021-04-18 15:39:34 -0400

    commit 18/apr
    Signed-off-by: rohi92 <gorthyrohith@gmail.com>

--- isAnagram.py
+++ isAnagram.py
@@ -1,22 +1,5 @@
-def isAnagram(self, s, t):
-    s1 = set(s)
-    s2 = set(t)
-    c1 = list(map(lambda x: [x, s.count(x)], s1))
-    c1 = sorted(c1, key=lambda x: x[0])
-    c2 = list(map(lambda x: [x, t.count(x)], s2))
-    c2 = sorted(c2, key=lambda x: x[0])
-    eff_listc1=[]
-    eff_listc2=[]
-    for i in c1:
-        if i not in eff_listc1:
-            eff_listc1.append(i)
-            for i in c2:
-                if i not in eff_listc2:
-                    eff_listc2.append(i)
-                    if eff_listc1 == eff_listc2 :
-                        return True
-                    else:
-                        return False
+def sample(self, s, t):
+    s=1
 
 if __name__=="__main__":
-    print(isAnagram("self","aa","a"))
+    print(sample("self","aa","a"))
