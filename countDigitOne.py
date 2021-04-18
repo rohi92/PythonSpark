a14269359b7b4632eaf142e367d431aad66c54dd
rohi92 <gorthyrohith@gmail.com>
2021-04-18 10:47:15 -0400

    Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
     Example 1:
    
     Input: n = 13
     Output: 6
    
    Signed-off-by: rohi92 <gorthyrohith@gmail.com>

---
+++ countDigitOne.py
@@ -0,0 +1,28 @@
+def countDigitOne(self, n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    count = 0
+    if n >= 3000000:
+        l1 = list(range(1, (n + 1 / 2), 1))
+        l2 = list(map(lambda x: str(x), l1))
+        l2 = list(filter(lambda x: '1' in x, l2))
+        l3 = list(map(lambda x: x.count('1'), l2))
+        count = count + sum(l3)
+        l1 = list(range((n + 1 / 2), n, 1))
+        l2 = list(map(lambda x: str(x), l1))
+        l2 = list(filter(lambda x: '1' in x, l2))
+        l3 = list(map(lambda x: x.count('1'), l2))
+        count = count + sum(l3)
+    else:
+        l1 = list(range(1, n + 1, 1))
+        l2 = list(map(lambda x: str(x), l1))
+        l2 = list(filter(lambda x: '1' in x, l2))
+        l3 = list(map(lambda x: x.count('1'), l2))
+    count = sum(l3)
+
+    return count
+
+if __name__=="__main__":
+    print(countDigitOne("self",13))
