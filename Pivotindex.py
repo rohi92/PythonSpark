dc8397b684a3b1c95ae887a749df75960980613c
rohi92 <82684425+rohi92@users.noreply.github.com>
2022-06-02 11:46:22 -0600

    Update Pivotindex.py

--- Pivotindex.py
+++ Pivotindex.py
@@ -1,3 +1,30 @@
+"""**Example1:**
+```
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+```
+**Example2:**
+```
+Input: nums = [2,1,-1]
+Output: 0
+Explanation:
+The pivot index is 0.
+Left sum = 0 (no elements to the left of index 0)
+Right sum = nums[1] + nums[2] = 1 + -1 = 0
+```
+**Example3:**
+```
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+```"""
+
 def get_pivot_index(nums):
     # *** write code here ***
     sum_index=[]
