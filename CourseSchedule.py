19643d488f87d6f92ba5bbb44be8b7f56291dba8
rohi92 <82684425+rohi92@users.noreply.github.com>
2023-03-14 22:44:35 +0000

    Commiting changes for the Course Schedule Leet problem

---
+++ CourseSchedule.py
@@ -0,0 +1,58 @@
+order_list=[]
+def bfs(dict,order_list,course_id):
+    try:
+        for i in course_id[1]:
+            if i not in order_list and course_id[0] not in order_list:
+                order_list.append(i)
+                order_list.append(course_id[0])
+
+            elif i not in order_list and course_id[0] in order_list:
+                index = order_list.index(i)
+                order_list.insert(index - 1, i)
+                bfs(dict, order_list, (i, dict[i]))
+            elif i in order_list and course_id[0] not in order_list:
+                index = order_list.index(i)
+                order_list.insert(index+1,course_id[0])
+
+    except:
+        return [0]
+
+
+
+
+
+
+
+
+def order(prerequisites,numCourses,dict):
+    try:
+
+
+        for k,v in dict.items():
+            bfs(dict,order_list,(k,v))
+        return order_list
+    except:
+        return [0]
+
+
+
+
+
+if __name__=="__main__":
+    numCourses , prerequisites =1, [[1,0],[2,0],[3,1],[3,2]]
+    dict={}
+    try:
+
+        for i in prerequisites:
+            try:
+                dict[i[0]].append(i[1])
+            except:
+                dict[i[0]]=[i[1]]
+        print(order(prerequisites, numCourses, dict))
+    except:
+        dict={}
+        print(order(prerequisites,numCourses,dict))
+
+
+
+
